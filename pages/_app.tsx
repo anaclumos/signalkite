9c867a9a5459d28644c71f074f02510ce4b893a0
Sunghyun Cho <mail@chosunghyun.com>
2023-03-26 16:20:54 -0700

    add prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
-import "nextra-theme-docs/style.css";
-import { Analytics } from '@vercel/analytics/react';
-import Script from "next/script";
+import 'nextra-theme-docs/style.css'
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <Script
-        src="https://sa.cho.sh/latest.js"
-        strategy="afterInteractive"
-      />
+      <Script src="https://sa.cho.sh/latest.js" strategy="afterInteractive" />
     </>
-  );
-};
-
+  )
+}
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 
-
-
 const Head = () => {
-  const { frontMatter, title } = useConfig();
+  const { frontMatter, title } = useConfig()
   return (
     <>
       <title>{title}</title>
@@ -14,8 +12,8 @@ const Head = () => {
       <meta name="og:type" content="website" />
       <meta name="og:url" content="https://hn.cho.sh" />
     </>
-  );
-};
+  )
+}
 
 const config: DocsThemeConfig = {
   logo: <strong>hn.cho.sh</strong>,
@@ -24,19 +22,25 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/anaclumos/hn.cho.sh/blob/main/',
   footer: {
-    text: <span>
-      MIT {new Date().getFullYear()} © <a href="https://cho.sh" target="_blank">Sunghyun Cho</a>.
-    </span>,
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://cho.sh" target="_blank">
+          Sunghyun Cho
+        </a>
+        .
+      </span>
+    ),
   },
   editLink: {
-    text: "Edit This Page on GitHub",
+    text: 'Edit This Page on GitHub',
   },
   toc: {
     float: true,
   },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s — hn.cho.sh'
+      titleTemplate: '%s — hn.cho.sh',
     }
   },
   head: <Head />,
@@ -45,41 +49,41 @@ const config: DocsThemeConfig = {
       return {
         light: 'Light',
         dark: 'Dark',
-        system: 'System'
+        system: 'System',
       }
-    }
+    },
   },
   i18n: [
-    { locale: 'bg', text: "български (bg)" },
-    { locale: 'cs', text: "Čeština (cs)" },
-    { locale: 'da', text: "Dansk (da)" },
-    { locale: 'de', text: "Deutsch (de)" },
-    { locale: 'el', text: "Ελληνικά (el)" },
-    { locale: 'en', text: "English (en)" },
-    { locale: 'es', text: "Espanya (es)" },
-    { locale: 'et', text: "Eesti (et)" },
-    { locale: 'fi', text: "Suomi (fi)" },
-    { locale: 'fr', text: "Français (fr)" },
-    { locale: 'hu', text: "Magyar (hu)" },
-    { locale: 'id', text: "Bahasa Indonesia (id)" },
-    { locale: 'it', text: "Italiano (it)" },
-    { locale: 'ja', text: "日本語 (ja)" },
-    { locale: 'ko', text: "한국어 (ko)" },
-    { locale: 'lt', text: "Lietuvių (lt)" },
-    { locale: 'lv', text: "Latviešu (lv)" },
-    { locale: 'nl', text: "Nederlands (nl)" },
-    { locale: 'no', text: "Norge (no)" },
-    { locale: 'pl', text: "Polski (pl)" },
-    { locale: 'pt', text: "Português (pt)" },
-    { locale: 'ro', text: "Română (ro)" },
-    { locale: 'ru', text: "Русский (ru)" },
-    { locale: 'sk', text: "Slovenčina (sk)" },
-    { locale: 'sl', text: "Slovenščina (sl)" },
-    { locale: 'sv', text: "Svenska (sv)" },
-    { locale: 'tr', text: "Türkçe (tr)" },
-    { locale: 'uk', text: "Українська (uk)" },
-    { locale: 'zh', text: "中文 (zh)" },
-  ]
+    { locale: 'bg', text: 'български (bg)' },
+    { locale: 'cs', text: 'Čeština (cs)' },
+    { locale: 'da', text: 'Dansk (da)' },
+    { locale: 'de', text: 'Deutsch (de)' },
+    { locale: 'el', text: 'Ελληνικά (el)' },
+    { locale: 'en', text: 'English (en)' },
+    { locale: 'es', text: 'Espanya (es)' },
+    { locale: 'et', text: 'Eesti (et)' },
+    { locale: 'fi', text: 'Suomi (fi)' },
+    { locale: 'fr', text: 'Français (fr)' },
+    { locale: 'hu', text: 'Magyar (hu)' },
+    { locale: 'id', text: 'Bahasa Indonesia (id)' },
+    { locale: 'it', text: 'Italiano (it)' },
+    { locale: 'ja', text: '日本語 (ja)' },
+    { locale: 'ko', text: '한국어 (ko)' },
+    { locale: 'lt', text: 'Lietuvių (lt)' },
+    { locale: 'lv', text: 'Latviešu (lv)' },
+    { locale: 'nl', text: 'Nederlands (nl)' },
+    { locale: 'no', text: 'Norge (no)' },
+    { locale: 'pl', text: 'Polski (pl)' },
+    { locale: 'pt', text: 'Português (pt)' },
+    { locale: 'ro', text: 'Română (ro)' },
+    { locale: 'ru', text: 'Русский (ru)' },
+    { locale: 'sk', text: 'Slovenčina (sk)' },
+    { locale: 'sl', text: 'Slovenščina (sl)' },
+    { locale: 'sv', text: 'Svenska (sv)' },
+    { locale: 'tr', text: 'Türkçe (tr)' },
+    { locale: 'uk', text: 'Українська (uk)' },
+    { locale: 'zh', text: '中文 (zh)' },
+  ],
 }
 
 export default config
