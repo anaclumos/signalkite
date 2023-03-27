6838eed6703def6b7c9610e3430ff703c2783418
Sunghyun Cho <mail@chosunghyun.com>
2023-03-27 01:02:43 -0700

    Add newsletter scheduling functionality with language-specific configurations and notification system using Resend and Psutil

---
+++ data/newsletter.py
@@ -0,0 +1,160 @@
+import requests
+from datetime import datetime
+import os
+from pytz import timezone
+from dotenv import load_dotenv
+from resend import Resend
+
+load_dotenv()
+
+server = "https://newsletters.cho.sh/api/campaigns"
+username = os.environ["NEWSLETTERS_CHO_SH_USERNAME"]
+password = os.environ["NEWSLETTERS_CHO_SH_PASSWORD"]
+resend = Resend(os.environ["RESEND_KEY"])
+notification = ""
+
+CONFIG = {
+    "zh": 34,
+    "uk": 33,
+    "tr": 32,
+    "sv": 31,
+    "sl": 30,
+    "sk": 29,
+    "ru": 28,
+    "ro": 27,
+    "pt": 26,
+    "pl": 25,
+    "no": 24,
+    "nl": 23,
+    "lv": 22,
+    "lt": 21,
+    "ko": 20,
+    "ja": 19,
+    "it": 18,
+    "id": 17,
+    "hu": 16,
+    "fr": 15,
+    "fi": 14,
+    "et": 13,
+    "es": 12,
+    "en": 11,
+    "el": 10,
+    "de": 9,
+    "da": 8,
+    "cs": 7,
+    "bg": 6,
+}
+
+
+def get_campaigns():
+    """Get all campaigns"""
+    return requests.get(server, auth=(username, password)).json()
+
+
+def create_campaign(title, body, lang):
+    """Create a new campaign"""
+    utc = timezone("UTC")
+    today = datetime.now().astimezone(utc).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    res = requests.post(
+        server,
+        auth=(username, password),
+        json={
+            "name": title,
+            "subject": title,
+            "type": "regular",
+            "content_type": "markdown",
+            "body": body,
+            "altbody": body,
+            "lists": [CONFIG[lang]],
+            "send_at": f"{today.strftime('%Y-%m-%d')}T08:00:00+00:00",
+        },
+    ).json()
+    requests.put(
+        server + "/" + str(res["data"]["id"]) + "/status",
+        auth=(username, password),
+        json={"status": "scheduled"},
+    )
+
+
+def find_today_newsletters(lang):
+    """Find the newsletter for today in the Research folder. All UTC."""
+    utc = timezone("UTC")
+    today = datetime.now().astimezone(utc).replace(hour=0, minute=0, second=0, microsecond=0)
+    filename = f"pages/{today.strftime('%Y/%m')}/{today.strftime('%d')}.{lang}.mdx"
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            body = f.read()
+            title = body.splitlines()[0].replace("# ", "") + " — hn.cho.sh ({lang}) "
+            body = "\n".join(body.splitlines()[1:])
+            body = body.replace("### ", "## ").replace("import { Steps } from 'nextra-theme-docs'", "").replace("<Steps>", "").replace("</Steps>", "")
+            return [(title, body)]
+
+
+def collect_device_info():
+    import platform
+    import psutil
+
+    collect_notification("\n\n\nCollecting device information...")
+    uname = platform.uname()
+    collect_notification(f"System: {uname.system}")
+    collect_notification(f"Node Name: {uname.node}")
+    collect_notification(f"Release: {uname.release}")
+    collect_notification(f"Version: {uname.version}")
+    collect_notification(f"Machine: {uname.machine}")
+    collect_notification(f"Processor: {uname.processor}")
+    collect_notification("Physical cores:", psutil.cpu_count(logical=False))
+    collect_notification("Total cores:", psutil.cpu_count(logical=True))
+    cpufreq = psutil.cpu_freq()
+    collect_notification(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+    collect_notification(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+    collect_notification(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+    collect_notification(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+
+def notify(title, text):
+    """Notify me about newsletter results."""
+    resend.send_email(
+        sender="notification@resend.dev",
+        to="hey@cho.sh",
+        subject=title,
+        text=text,
+    )
+
+
+def collect_notification(*args):
+    args = [str(arg) for arg in args]
+    text = " ".join(args)
+    """Collect notifications and send them at the end."""
+    global notification
+    notification += text + "\n"
+
+
+def schedule_newsletter(lang):
+    """Schedule the newsletter for today."""
+    newsletters = find_today_newsletters(lang)
+    if newsletters:
+        campaigns = get_campaigns()["data"]["results"]
+        for title, post in newsletters:
+            duplicate = False
+            for campaign in campaigns:
+                if title in campaign["name"]:
+                    collect_notification("Duplicate... ", title)
+                    duplicate = True
+            if not duplicate:
+                collect_notification("Scheduling... ", title)
+                create_campaign(title, post, lang)
+    else:
+        collect_notification("No newsletter for today.")
+
+
+
+
+if __name__ == "__main__":
+    for lang in CONFIG:
+        schedule_newsletter(lang)
+    collect_device_info()
+    notify(
+        f"Newsletter Job {datetime.now().astimezone(timezone('US/Pacific')).strftime('%Y-%m-%d %H:%M:%S')}",
+        notification,
+    )
