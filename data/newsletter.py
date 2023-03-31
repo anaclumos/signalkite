f9cda0690ffd20d5759600e171abc202de7f293c
Sunghyun Cho <mail@chosunghyun.com>
2023-03-31 02:46:21 -0700

    improve i18n

--- data/newsletter.py
+++ data/newsletter.py
@@ -46,6 +46,37 @@ CONFIG = {
     "bg": 6,
 }
 
+EMAIL_CTA = {
+    'bg': 'Хареса ли ви статията? Споделете я с техническите си приятели по света и в многоезичните форуми! 😄🌏🚀',
+    'cs': 'Líbil se vám článek? Podělte se o něj s kamarády techniky po celém světě a na vícejazyčných fórech! 😄🌏🚀',
+    'da': 'Elskede du artiklen? Del den med tech-venner verden over og i flersprogede fora! 😄🌏🚀',
+    'de': 'Hat Ihnen der Artikel gefallen? Teilen Sie ihn mit Technikfreunden weltweit und in mehrsprachigen Foren! 😄🌏🚀',
+    'el': 'Σας άρεσε το άρθρο; Μοιραστείτε το με φίλους τεχνολογίας σε όλο τον κόσμο και σε πολύγλωσσα φόρουμ! 😄🌏🚀',
+    'es': '¿Te ha gustado el artículo? Compártelo con tus amigos de la tecnología de todo el mundo y en foros multilingües. 😄🌏🚀',
+    'et': 'Armastasite artiklit? Jaga seda tehnikavendadega üle maailma ja mitmekeelsetes foorumites! 😄🌏🚀',
+    'en': 'Loved the article? Share it with tech pals worldwide and multilingual forums! 😄🌏🚀',
+    'fi': 'Rakastitko artikkelia? Jaa se tekniikkakavereiden kanssa maailmanlaajuisesti ja monikielisillä foorumeilla! 😄🌏🚀',
+    'fr': "Vous avez aimé l'article ? Partagez-le avec vos amis techniciens du monde entier et les forums multilingues ! 😄🌏🚀",
+    'hu': 'Tetszett a cikk? Oszd meg a tech haverokkal világszerte és a többnyelvű fórumokon! 😄🌏🚀',
+    'id': 'Anda menyukai artikel ini? Bagikan dengan teman-teman teknologi di seluruh dunia dan forum multibahasa! 😄🌏🚀',
+    'it': "Ti è piaciuto l'articolo? Condividilo con gli amici tecnologici di tutto il mondo e con i forum multilingue! 😄🌏🚀",
+    'ja': 'この記事を気に入りましたか？世界中の技術仲間や多言語フォーラムで共有しましょう！私たちのグローバルヒーローになりましょう！😄🌏🚀',
+    'ko': '기사가 마음에 드셨나요? 전 세계 기술 동료 및 다국어 포럼과 공유해주세요! 😄🌏🚀',
+    'lt': 'Patiko straipsnis? Pasidalykite juo su technikos bičiuliais visame pasaulyje ir daugiakalbiuose forumuose! 😄🌏🚀',
+    'lv': 'Patika raksts? Dalieties tajā ar tehnoloģiju draugiem visā pasaulē un daudzvalodu forumos! 😄🌏🚀',
+    'nl': 'Hield u van het artikel? Deel het met technische vrienden wereldwijd en meertalige forums! 😄🌏🚀',
+    'nb': 'Likte du artikkelen? Del den med tech-venner over hele verden og i flerspråklige fora! 😄🌏🚀',
+    'pl': 'Podobał Ci się ten artykuł? Podziel się nim z tech-przyjaciółmi na całym świecie i na wielojęzycznych forach! 😄🌏🚀',
+    'pt': 'Adorou o artigo? Partilhe-o com amigos técnicos de todo o mundo e fóruns multilingues! 😄🌏🚀',
+    'ro': 'Ți-a plăcut articolul? Împărtășiți-l cu prietenii de tehnologie din întreaga lume și pe forumurile multilingve! 😄🌏🚀',
+    'ru': 'Понравилась статья? Поделитесь ею с друзьями-техниками по всему миру и на многоязычных форумах! 😄🌏🚀',
+    'sk': 'Páčil sa vám článok? Podeľte sa oň s kamarátmi z technického sveta a viacjazyčných fór! 😄🌏🚀',
+    'sl': 'Ali vam je bil članek všeč? Delite ga s tehničnimi prijatelji po vsem svetu in na večjezičnih forumih! 😄🌏🚀',
+    'sv': 'Gillade du artikeln? Dela den med tekniska vänner världen över och i flerspråkiga forum! 😄🌏🚀',
+    'tr': 'Makaleyi beğendiniz mi? Dünya çapındaki teknoloji dostlarınızla ve çok dilli forumlarda paylaşın! 😄🌏🚀',
+    'uk': 'Сподобалася стаття? Поділіться нею з технічними друзями по всьому світу та на багатомовних форумах! 😄🌏🚀',
+    'zh': '喜欢这篇文章吗？与世界各地的技术伙伴和多语言论坛分享吧!😄🌏🚀'
+}
 
 def get_campaigns():
     """Get all campaigns"""
@@ -56,7 +87,13 @@ def create_campaign(title, body, lang):
     """Create a new campaign"""
     utc = timezone("UTC")
     today = datetime.now().astimezone(utc).replace(hour=0, minute=0, second=0, microsecond=0)
-    next_hour = datetime.now().astimezone(utc).replace(minute=0, second=0, microsecond=0) + timedelta(minutes=CONFIG[lang] * 3 + 60)
+    next_hour = datetime.now().astimezone(utc).replace(minute=0, second=0, microsecond=0) + timedelta(minutes=CONFIG[lang] * 3 + 30)
+
+    url = 'https://hn.cho.sh/' + lang + '/' + today.strftime('%Y/%m/%d')
+
+    md_link = f"""\n\n[hn.cho.sh/{lang}/{today.strftime('%Y/%m/%d')}]({url})\n\n"""
+
+    prefix = EMAIL_CTA[lang] + md_link
 
     res = requests.post(
         server,
@@ -66,8 +103,8 @@ def create_campaign(title, body, lang):
             "subject": title,
             "type": "regular",
             "content_type": "markdown",
-            "body": body,
-            "altbody": body,
+            "body": prefix + body,
+            "altbody": prefix +  body,
             "lists": [CONFIG[lang]],
             "send_at": f"{today.strftime('%Y-%m-%d')}T{next_hour.strftime('%H:%M:%S')}+00:00",
         },
@@ -88,8 +125,7 @@ def find_today_newsletters(lang):
         with open(filename, "r") as f:
             body = f.read()
             title = format_date(today, format="long", locale=lang) + " — hn.cho.sh/" + lang
-            body = "\n".join(body.splitlines()[1:])
-            body = body.replace("### ", "## ").replace("import { Steps } from 'nextra-theme-docs'", "").replace("<Steps>", "").replace("</Steps>", "").replace("import CallToAction from '../../../components/CallToAction'", "").replace("<CallToAction />", "")
+            body = body.replace("import { Steps } from 'nextra-theme-docs'", "").replace("<Steps>", "").replace("</Steps>", "").replace("import CallToAction from '../../../components/CallToAction'", "").replace("<CallToAction />", "")
             return [(title, body)]
 
 
---
+++ data/oneliner.py
@@ -0,0 +1,61 @@
+import sys
+import deepl
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+from pytz import timezone
+from babel.dates import format_date
+
+load_dotenv()
+
+language = [
+    { 'locale': 'bg', 'filename_locale': 'bg',  'text': 'български (bg)' },
+    { 'locale': 'cs', 'filename_locale': 'cs',  'text': 'Čeština (cs)' },
+    { 'locale': 'da', 'filename_locale': 'da',  'text': 'Dansk (da)' },
+    { 'locale': 'de', 'filename_locale': 'de',  'text': 'Deutsch (de)' },
+    { 'locale': 'el', 'filename_locale': 'el',  'text': 'Ελληνικά (el)' },
+    { 'locale': 'en', 'filename_locale': 'en',  'text': 'English (en)' },
+    { 'locale': 'es', 'filename_locale': 'es',  'text': 'Espanya (es)' },
+    { 'locale': 'et', 'filename_locale': 'et',  'text': 'Eesti (et)' },
+    { 'locale': 'fi', 'filename_locale': 'fi',  'text': 'Suomi (fi)' },
+    { 'locale': 'fr', 'filename_locale': 'fr',  'text': 'Français (fr)' },
+    { 'locale': 'hu', 'filename_locale': 'hu',  'text': 'Magyar (hu)' },
+    { 'locale': 'id', 'filename_locale': 'id',  'text': 'Bahasa Indonesia (id)' },
+    { 'locale': 'it', 'filename_locale': 'it',  'text': 'Italiano (it)' },
+    { 'locale': 'ja', 'filename_locale': 'ja',  'text': '日本語 (ja)' },
+    { 'locale': 'ko', 'filename_locale': 'ko',  'text': '한국어 (ko)' },
+    { 'locale': 'lt', 'filename_locale': 'lt',  'text': 'Lietuvių (lt)' },
+    { 'locale': 'lv', 'filename_locale': 'lv',  'text': 'Latviešu (lv)' },
+    { 'locale': 'nl', 'filename_locale': 'nl',  'text': 'Nederlands (nl)' },
+    { 'locale': 'nb', 'filename_locale': 'nb',  'text': 'Bokmål (nb)' },
+    { 'locale': 'pl', 'filename_locale': 'pl',  'text': 'Polski (pl)' },
+    { 'locale': 'PT-PT','filename_locale': 'pt' , 'text': 'Português (pt)' },
+    { 'locale': 'ro', 'filename_locale': 'ro',  'text': 'Română (ro)' },
+    { 'locale': 'ru', 'filename_locale': 'ru',  'text': 'Русский (ru)' },
+    { 'locale': 'sk', 'filename_locale': 'sk',  'text': 'Slovenčina (sk)' },
+    { 'locale': 'sl', 'filename_locale': 'sl',  'text': 'Slovenščina (sl)' },
+    { 'locale': 'sv', 'filename_locale': 'sv',  'text': 'Svenska (sv)' },
+    { 'locale': 'tr', 'filename_locale': 'tr',  'text': 'Türkçe (tr)' },
+    { 'locale': 'uk', 'filename_locale': 'uk',  'text': 'Українська (uk)' },
+    { 'locale': 'zh', 'filename_locale': 'zh',  'text': '中文 (zh)' },
+  ]
+
+if __name__ == "__main__":
+    # ask for user input in English
+    print("Please enter the text you want to translate:")
+    text = input()
+
+    # translate the text to all languages as json and print back
+    translator = deepl.Translator(os.getenv("DEEPL_API_KEY"))
+    answer = {'en': text}
+    for lang in language:
+        if lang['locale'] == 'en':
+            continue
+        print("Translating to " + lang['text'])
+        # translate the text
+        result = translator.translate_text(text, target_lang=lang['locale'], source_lang='EN')
+        # add the translation to the answer
+        answer[lang['filename_locale']] = result.text
+    
+    # print the answer
+    print(answer)
