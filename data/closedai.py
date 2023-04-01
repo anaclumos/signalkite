c21b0a24428c42dec3c0196e0258f3a91bb26661
Sunghyun Cho <mail@chosunghyun.com>
2023-03-31 17:43:27 -0700

    Refactor code to improve readability and follow best practices

--- data/closedai.py
+++ data/closedai.py
@@ -8,6 +8,21 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 OPENAI_RETRY_COUNT = 5
 
+SITUATION = """
+You are an editor at 'The Tech Times', an expert journalist for cutting-edge tech News.
+I will give you the raw text content.
+You must provide a concise summary in mutually exclusive but collectively complete sentences.
+Please understand that some comments may include sarcasm, and you must figure out that it's not the central argument or factual.
+
+You must understand that the primary readers of this post are experts in the field, and they do not want to read the same thing they already know.
+You must consider this question: What is the most important thing people should know about this post? Why is this post special? Is there something new or exciting thing going on? Did something get released? Why is this post, out of all the posts on Hacker News, suddenly getting so much attention? What made such tech-savvy people suddenly interested in this post? Your job is to capture vital points that interest the readers.
+Do not repeat dull facts, such as 'The post is about a new technology'.
+
+You must also know that you should be 'confident but not arrogant'.
+It would be best not to use expressions like "this may be..." "potentially,"... or such.
+Things should be 'clearly' and 'obviously' stated.
+"""
+
 
 def shorten(text: str, limit: int, title="") -> str:
     summary = ""
@@ -23,21 +38,11 @@ def shorten(text: str, limit: int, title="") -> str:
                     {
                         "role": "user",
                         "content": f"""
-
+{SITUATION}
                         
-You are an editor at 'The Tech Times', a Expert Journalist for Cutting-Edge Tech News.
-You were writing an article, but found that the article is too long, so you decided to summarize it.
-I will give you the raw text content. Your job is to give a concise summary in mutually exclusive but collectively exhaustive sentences.
-
-You must understand that the primary readers of this post are experts in the field, and they do not what to read the same thing that they already know.
-You must keep this question in mind: What is the most important thing people should know about this post? Why is this post special?
-Is there something new or interesting thing going on?
-Why is this post, out of all the posts on Hacker News, suddenly getting so much attention?
-What made such tech-savvy people suddenly interested in this post?
-
 Hard limit {limit // 16} words.
-Please understand that some comments may include sarcasm, and you must understand it's not the main point. 
-If there is an esoteric arcane word that aptly describes the sentence, utilize it to make the sentence shorter.
+Please understand that some comments may include sarcasm, and you must realize it's not the main point.
+If a profound, arcane word aptly describes the sentence, utilize it to make the sentence shorter.
 
 Text: {w}
 """,
@@ -58,7 +63,7 @@ Text: {w}
 def bulletpoint_summarize(title, text):
     summary = ""
     try:
-        print(f"Creating Summary for...   {title}")
+        print(f"Creating Summary for... {title}")
         sleep(1)  # OpenAI has a rate limit of 60 requests per minute for pay-as-you-go users
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -66,32 +71,18 @@ def bulletpoint_summarize(title, text):
                 {
                     "role": "user",
                     "content": f"""
-
-You are an editor at 'The Tech Times', a Expert Journalist for Cutting-Edge Tech News.
-I will give you the raw text content. Your job is to give a concise summary in mutually exclusive but collectively exhaustive sentences.
-Please understand that some comments may include sarcasm, and you must figure out that it's not the main argument or factual.
-
-You must understand that the primary readers of this post are experts in the field, and they do not what to read the same thing that they already know.
-You must keep this question in mind: What is the most important thing people should know about this post? Why is this post special?
-Is there something new or interesting thing going on?
-Why is this post, out of all the posts on Hacker News, suddenly getting so much attention?
-What made such tech-savvy people suddenly interested in this post?
-
-Your job is to capture such key points, that would make the readers interested.
-Do not repeat dull facts, such as 'The post is about a new technology'.
-
-You must also be aware that you should be 'confident but not arrogant'.
-You must not use expressions like "this may be..." "potentially"... or such.
-Things should be 'clearly' and 'obviously' stated.
-
-Use markdown syntax wherever possible, such as making quotes or bold texting, or in-line codes.
-It must be a freeform text, not bullet points. It must be grammatically correct and polite.
-Each sentence must end with a punctuation, such as a period. The title of this post is '{title}'.
+{SITUATION}
+                    
+Use markdown syntax wherever possible, such as making quotes, bold texting, or in-line codes.
+It must be a freeform text, not bullet points.
+It must be grammatically correct and polite.
+Each sentence must end with punctuation, such as a period.
+The title of this post is '{title}'.
 
 Now, I will give you the text.
 Summarize in markdown, less than 100 tokens.
-Text: {text}""",
-
+Text: {text}
+""",
                 },
             ],
         )
@@ -133,29 +124,17 @@ def get_title(title, text):
                     {
                         "role": "user",
                         "content": f"""
-You are an editor at 'The Tech Times', a Expert Journalist for Cutting-Edge Tech News.
-I will give you the raw text content. Your job is to give a concise title for the following post.
-
-You must understand that the primary readers of this post are experts in the field, and they do not what to read the same thing that they already know.
-You must keep this question in mind: What is the most important thing people should know about this post? Why is this post special?
-Is there something new or interesting thing going on?
-Why is this post, out of all the posts on Hacker News, suddenly getting so much attention?
-What made such tech-savvy people suddenly interested in this post?
-
-Your job is to capture such key points, that would make the readers interested.
-Do not repeat dull facts, such as 'The post is about a new technology'.
+{SITUATION}
 
 Capitalize in Chicago style.
-you must keep the proper nouns, acronyms, scientific terms, and registered trademarks as is,
-such as 'iCloud', 'macOS', 'GPT', 'US', 'FBI', 'EU', 'CFTC', 'LLMs', 'GPTs', 'HN', 'Q&A', 'OpenPGP', 'SSH', 'PDF', 'PH.D.', '$3M', and so on.
-
+You must keep the proper nouns, acronyms, scientific terms, and registered trademarks as is, such as 'iCloud', 'macOS', 'GPT', 'US', 'FBI', 'EU', 'CFTC', 'LLMs', 'GPTs', 'HN', 'Q&A', 'OpenPGP', 'SSH', 'PDF', 'PH.D.', '$3M', and so on.
 Do not add suffixes, such as '| Website'.
-
-Ignore Website saying You Need JavaScript; That's not the important part.
-No period at the end.
-
-Please understand that some comments may include sarcasm, and you must understand it's not the main point.
-The original title was {title}. HARD LIMIT 10 WORDS. Text: {text}
+Ignore the Website saying You Need JavaScript; That's not the important part.
+No period in the end.
+Please understand that some comments may include sarcasm, and you must realize it's not the main point.
+The original title was {title}.
+HARD LIMIT 10 WORDS.
+Text: {text}
 
 What would be a better title for this post?
 """,
