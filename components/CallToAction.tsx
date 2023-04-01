08f6e064b1814820bdd075c93f37cf2edef0611c
Sunghyun Cho <mail@chosunghyun.com>
2023-03-31 20:30:25 -0700

    Refactor share function to accept language as a parameter in CallToAction component

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -130,9 +130,7 @@ const i18n = {
   },
 }
 
-const share = () => {
-  const router = useRouter()
-  const language = router.locale || 'en'
+const share = (language) => {
   if (navigator.share) {
     navigator.share({
       title: document.title,
@@ -158,7 +156,7 @@ const CallToAction = () => {
   return (
     <Cards>
       <div
-        onClick={share}
+        onClick={() => share(language)}
         className="nextra-card nx-group nx-flex nx-flex-col nx-justify-start nx-overflow-hidden nx-rounded-lg nx-border nx-border-gray-200 nx-text-current nx-no-underline dark:nx-shadow-none hover:nx-shadow-gray-100 dark:hover:nx-shadow-none nx-shadow-gray-100 active:nx-shadow-sm active:nx-shadow-gray-200 nx-transition-all nx-duration-200 hover:nx-border-gray-300 nx-bg-transparent nx-shadow-sm dark:nx-border-neutral-800 hover:nx-bg-slate-50 hover:nx-shadow-md dark:hover:nx-border-neutral-700 dark:hover:nx-bg-neutral-900 nx-cursor-pointer"
       >
         <span className="nx-flex nx-font-semibold nx-items-start nx-gap-2 nx-p-4 nx-text-gray-700 hover:nx-text-gray-900 dark:nx-text-neutral-200 dark:hover:nx-text-neutral-50">
