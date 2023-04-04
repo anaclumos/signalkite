2919f99e6276420b61a1f61605d3480abef3f4e6
Sunghyun Cho <mail@chosunghyun.com>
2023-04-03 22:49:12 -0700

    Add next-sitemap package and configuration for multi-language site support

---
+++ next-sitemap.config.js
@@ -0,0 +1,123 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hn.cho.sh',
+  generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      hreflang: 'bg',
+      href: 'https://hn.cho.sh/bg'
+    },
+    {
+      hreflang: 'cs',
+      href: 'https://hn.cho.sh/cs'
+    },
+    {
+      hreflang: 'da',
+      href: 'https://hn.cho.sh/da'
+    },
+    {
+      hreflang: 'de',
+      href: 'https://hn.cho.sh/de'
+    },
+    {
+      hreflang: 'el',
+      href: 'https://hn.cho.sh/el'
+    },
+    {
+      hreflang: 'en',
+      href: 'https://hn.cho.sh/'
+    },
+    {
+      hreflang: 'es',
+      href: 'https://hn.cho.sh/es'
+    },
+    {
+      hreflang: 'et',
+      href: 'https://hn.cho.sh/et'
+    },
+    {
+      hreflang: 'fi',
+      href: 'https://hn.cho.sh/fi'
+    },
+    {
+      hreflang: 'fr',
+      href: 'https://hn.cho.sh/fr'
+    },
+    {
+      hreflang: 'hu',
+      href: 'https://hn.cho.sh/hu'
+    },
+    {
+      hreflang: 'id',
+      href: 'https://hn.cho.sh/id'
+    },
+    {
+      hreflang: 'it',
+      href: 'https://hn.cho.sh/it'
+    },
+    {
+      hreflang: 'ja',
+      href: 'https://hn.cho.sh/ja'
+    },
+    {
+      hreflang: 'ko',
+      href: 'https://hn.cho.sh/ko'
+    },
+    {
+      hreflang: 'lt',
+      href: 'https://hn.cho.sh/lt'
+    },
+    {
+      hreflang: 'lv',
+      href: 'https://hn.cho.sh/lv'
+    },
+    {
+      hreflang: 'nb',
+      href: 'https://hn.cho.sh/nb'
+    },
+    {
+      hreflang: 'nl',
+      href: 'https://hn.cho.sh/nl'
+    },
+    {
+      hreflang: 'pl',
+      href: 'https://hn.cho.sh/pl'
+    },
+    {
+      hreflang: 'pt',
+      href: 'https://hn.cho.sh/pt'
+    },
+    {
+      hreflang: 'ro',
+      href: 'https://hn.cho.sh/ro'
+    },
+    {
+      hreflang: 'ru',
+      href: 'https://hn.cho.sh/ru'
+    },
+    {
+      hreflang: 'sk',
+      href: 'https://hn.cho.sh/sk'
+    },
+    {
+      hreflang: 'sl',
+      href: 'https://hn.cho.sh/sl'
+    },
+    {
+      hreflang: 'sv',
+      href: 'https://hn.cho.sh/sv'
+    },
+    {
+      hreflang: 'tr',
+      href: 'https://hn.cho.sh/tr'
+    },
+    {
+      hreflang: 'uk',
+      href: 'https://hn.cho.sh/uk'
+    },
+    {
+      hreflang: 'zh',
+      href: 'https://hn.cho.sh/zh'
+    },
+  ],
+}
