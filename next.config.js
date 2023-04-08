d746f4aa84c079f13f214bd08f4fbcc496c2a4e0
Sunghyun Cho <mail@chosunghyun.com>
2023-04-08 15:59:40 -0700

    Update i18n settings to enable locale detection

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   i18n: {
     defaultLocale: 'en',
-    useBrowserDefault: true,
+    localeDetection: true,
     locales: [
       'en',
       'bg',
