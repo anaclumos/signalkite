ef7c66ff48e9bb5522b5906adeb4c0da6d439af4
Sunghyun Cho <mail@chosunghyun.com>
2023-06-21 23:28:55 -0700

    Add Clerk authentication to App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,6 @@
+import './index.css'
+import './i18n'
+
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 import * as theme from 'config/chakra.config'
 
@@ -7,20 +10,21 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import './index.css'
-import './i18n'
+import { AuthProvider, useAuth } from './auth'
 
 const extendedTheme = extendTheme(theme)
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle @ %AppTitle">
-      <ColorModeScript />
-      <ChakraProvider theme={extendedTheme}>
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </ChakraProvider>
+      <AuthProvider>
+        <ColorModeScript />
+        <ChakraProvider theme={extendedTheme}>
+          <RedwoodApolloProvider useAuth={useAuth}>
+            <Routes />
+          </RedwoodApolloProvider>
+        </ChakraProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,14 +1,23 @@
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Private, Route, Router, Set } from '@redwoodjs/router'
 
 import AppLayout from 'src/layouts/AppLayout/AppLayout'
 
+import { useAuth } from './auth'
+
 const Routes = () => {
   return (
-    <Router>
+    <Router useAuth={useAuth}>
       <Set wrap={AppLayout}>
+        <Private unauthenticated="fallback">
+          <Route path="/settings" page={SettingsPage} name="settings" />
+          <Route path="/settings/{locale:String}" page={SettingsPage} name="settings" />
+          <Route path="/profile/" page={ProfilePage} name="profile" />
+          <Route path="/profile/{locale:String}" page={ProfilePage} name="profile" />
+        </Private>
         <Route path="/" page={HomePage} name="home" />
         <Route path="/{locale:String}" page={HomePage} name="home" />
       </Set>
+      <Route path="/fallback" page={FallbackPage} name="fallback" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
