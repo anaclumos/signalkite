36733d968f03b2b3ff8e020ac1f500c157cc8cf7
Sunghyun Cho <mail@chosunghyun.com>
2023-06-21 23:28:07 -0700

    Add locale to localStorage and navigate to home:

--- web/src/hooks/useLocaleRedirect.ts
+++ web/src/hooks/useLocaleRedirect.ts
@@ -5,17 +5,20 @@ import { useTranslation } from 'react-i18next'
 import { navigate, routes } from '@redwoodjs/router'
 
 import { allowedLocalesList } from 'src/i18n'
+
 export const useLocaleRedirect = ({ locale }) => {
   const { i18n } = useTranslation()
   useEffect(() => {
     i18n.changeLanguage(locale)
+    window.localStorage.setItem('locale', locale)
+    navigate(routes.home({ locale: locale }))
   }, [locale, i18n])
   if (!locale || !allowedLocalesList.includes(locale)) {
-    console.warn(
-      `Invalid locale '${locale}' detected. Redirecting to default locale.`
-    )
-    const navlang = navigator.language?.split('-')[0] ?? 'en'
+    const navlang =
+      window.localStorage.getItem('locale') ??
+      window.location.pathname.split('/')[-1] ??
+      navigator.language?.split('-')[0]
     const redirectLocale = allowedLocalesList.includes(navlang) ? navlang : 'en'
-    navigate(routes.home({ locale: redirectLocale }))
+    locale = redirectLocale
   }
 }
