c5d8514cf7d46318874a11de975df1201a419052
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-06-26 20:18:08 -0700

    Change authDecoder import to clerkAuthDecoder

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -1,4 +1,4 @@
-import { authDecoder } from '@redwoodjs/auth-clerk-api'
+import { clerkAuthDecoder as authDecoder } from '@redwoodjs/auth-clerk-api'
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
 
 import directives from 'src/directives/**/*.{js,ts}'
