2475fb0f62da66cba97f9405c085ac69a2b3c447
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-06-27 22:25:20 -0700

    Update link summaries with generated bullet points

--- scripts/generateGptLinkSummary.ts
+++ scripts/generateGptLinkSummary.ts
@@ -9,7 +9,7 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
 export default async () => {
   const linkSummaries = await db.linkSummary.findMany({
-    where: { linkSummary: undefined, body: { not: undefined } },
+    where: { linkSummary: '', body: { not: '' } },
     take: 5,
   })
 
@@ -36,11 +36,40 @@ export default async () => {
             `You are  an expert journalist for cutting-edge tech News. You must provide a concise summary in mutually exclusive but collectively complete bullet points. Please understand that some comments may include sarcasm, and you must figure out that it's not the central argument or factual. Remain neutral and objective. Write the summary as if you are explaining to a university student or an entry-level software engineer. The primary readers of this post are new to the industry and would want some background context. You must consider this question: What is the most important thing people should know about this post? Why is this post special? Is there something new or exciting thing going on? Did something get released? What made such tech-savvy people suddenly interested in this post? Your job is to capture vital points that interest the readers. If there is no meaningful content, for example, if it looks like a simple error message, simply print "N/A."
 
             Use markdown syntax wherever possible, such as making quotes, bold texting, or in-line codes. It must be a bullet point list, not a freeform text; that is, start with '-' immediately followed by a space. Therefore, it will look like '- '. Each bullet should terminate with one return '\n'. Do not change line twice between bullets. It must be grammatically correct and polite. You must write the summary as if you are explaining to a middle schooler. Explain all jargons and acronyms. Employ transitioning phrases and native arguments, but concise and succinct. If you get a message that it requires a security access check, or the website is unreachable, simply print "N/A." Now, I will give you the text.
+
+            For example:
+
+            Text:
+            "Major publishing companies are bombarding volunteers who operate IPFS gateways with tens of thousands of DMCA notices, despite knowing that these volunteers are not responsible for the content and cannot take it down. One gateway operator has already shut down their service due to the pressure. The notices are being sent to abuse addresses at the host of the gateways, rather than directly to the volunteers. The notices demand the takedown of thousands of URLs that have nothing to do with the volunteers and are often not even accessible. This demonstrates that IPFS, although technically resilient against censorship, can still be affected by self-censorship due to the pressure from copyright complaints."
+
+            Output:
+            '- Major publishing companies are sending a large number of DMCA notices to volunteers who run IPFS gateways.\n'
+            '- These volunteers are not responsible for the content and cannot remove it, but they are being targeted anyway.\n'
+            '- One gateway operator has already stopped their service because of this pressure.\n'
+            '- Despite its technical resilience against censorship, the IPFS network can still be affected by chilling effects and censorship through DMCA notices.\n'
+            '- Evidence suggests that some of the URLs targeted in the notices have never been accessed or even worked.',
             `
           ),
           new HumanChatMessage(`Summarize: ${summary}`),
         ])
-        console.log({ response })
+
+        const { text } = response
+        let arr = text.split('\n')
+
+        arr = arr.map((item) => item.trim())
+        arr = arr.map((item) => {
+          if (item.startsWith('- ')) {
+            item = item.substring(2)
+          }
+          return item
+        })
+
+        await db.linkSummary.update({
+          where: { id: linkSummary.id },
+          data: {
+            linkSummary: JSON.stringify(arr),
+          },
+        })
       } catch (e) {
         console.error(e)
       }
