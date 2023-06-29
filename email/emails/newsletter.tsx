67fd4632fa7ca3a143097cbc32a1f8195b9eabf5
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-06-29 16:52:01 -0700

    Add newsletter email component file

---
+++ email/emails/newsletter.tsx
@@ -0,0 +1,161 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components'
+
+import * as React from 'react'
+
+type NewsletterEmailProps = {
+  title?: string
+  content: {
+    headline: string
+    link: string
+    bullets: string[]
+    commentLink: string
+    commentBullets: string[]
+  }[]
+}
+
+export const NewsletterEmail = ({
+  title = 'Hacker News Daily',
+  content = [
+    {
+      headline: 'Google has a secret browser hidden inside the settings',
+      link: 'https://matan-h.com/google-has-a-secret-browser-hidden-inside-the-settings/',
+      bullets: [
+        'A hidden browser has been found within the settings of Android apps.',
+        'The browser can be accessed through the "Manage my account" section of the settings.',
+        'It has no browsing history and automatically logs out of Google accounts.',
+        'The browser has some potentially dangerous functions that could be exploited by malicious websites.',
+        'It bypasses parental controls and can play YouTube videos, albeit with ads.',
+        'The discovery has been reported to Google, who claims it is not a security vulnerability.',
+      ],
+      commentLink: 'https://news.ycombinator.com/item?id=36478206',
+      commentBullets: [
+        'The passage covers multiple topics related to security vulnerabilities, bug reporting, user behavior, threat modeling, and parental controls.',
+        "It discusses the definition and scope of information security and the role of technology in children's lives.",
+        'User behavior and intentions are crucial factors to consider when designing security measures.',
+        'Ongoing evaluation and improvement are necessary for effective security measures.',
+      ],
+    },
+    {
+      headline: 'The hidden cost of air quality monitoring',
+      link: 'https://www.airgradient.com/blog/hidden-costs-of-air-quality-monitoring/',
+      bullets: [
+        'Some air quality monitoring manufacturers are raising prices for customers through various tactics.',
+        'These tactics include using proprietary sensor modules, creating ecosystems that lock users in, building monitors with short lifetimes, and having unclear data ownership policies.',
+        'However, AirGradient, a self-funded startup, is taking a different approach.',
+        'AirGradient prioritizes sustainability, honesty, and open-source monitors.',
+        'Potential buyers are encouraged to ask specific questions about data ownership, data monetization, repairability, and spare parts availability before buying a monitor.',
+      ],
+      commentLink: 'https://news.ycombinator.com/item?id=36499905',
+      commentBullets: [
+        'Air quality monitoring is crucial for underserved communities, and AirGradient aims to provide affordable solutions.',
+        'Some commenters question the accuracy and reliability of low-cost monitoring devices.',
+        'The role of plants and trees in reducing air pollution is discussed, as well as regulations on gas-powered lawn equipment and the environmental impact of leaf blowers.',
+        'Transitioning to electric-powered tools and vehicles is considered a solution for reducing air pollution.',
+        'Users debate the features and integration of different air quality monitoring devices and platforms.',
+        'Limitations of current systems and potential improvements are discussed.',
+        'Migrant workers and blue-collar commuters face challenges accessing relevant air quality data.',
+        'Water quality monitoring and the need for raising awareness to find solutions are also mentioned.',
+      ],
+    },
+  ],
+}: NewsletterEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>
+      {content?.[0]?.headline || title || 'Here is your weekly newsletter'}
+    </Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Heading as="h1">{title}</Heading>
+        {content?.map((section, sectionIndex) => (
+          <>
+            <Hr style={hr} />
+            <Link href={section.link}>
+              <Heading as="h2" key={`${sectionIndex}-headline`}>
+                {section.headline}
+              </Heading>
+            </Link>
+            <ul
+              style={{
+                lineHeight: '1.5',
+                color: '#484848',
+                listStylePosition: 'outside',
+                paddingInlineStart: '20px',
+              }}
+            >
+              {section.bullets.map((bullet, bulletIndex) => (
+                <li key={`${sectionIndex}-${bulletIndex}`}>{`${bullet}`}</li>
+              ))}
+            </ul>
+            <Link href={section.commentLink}>
+              <Heading as="h3" key={`${sectionIndex}-comment`}>
+                Comments
+              </Heading>
+            </Link>
+            <ul
+              style={{
+                lineHeight: '1.5',
+                color: '#484848',
+                listStylePosition: 'outside',
+                paddingInlineStart: '20px',
+              }}
+            >
+              {section.commentBullets.map((bullet, bulletIndex) => (
+                <li key={`${sectionIndex}-${bulletIndex}`}>{`${bullet}`}</li>
+              ))}
+            </ul>
+          </>
+        ))}
+        <Hr style={hr} />
+        <Link href="https://github.com/anaclumos/heimdall" style={footer}>
+          Sunghyun Cho from ★ Starfish, Co.
+        </Link>
+      </Container>
+    </Body>
+  </Html>
+)
+
+export default NewsletterEmail
+
+const main = {
+  backgroundColor: '#ffffff',
+  fontFamily:
+    "ui-sans-serif, -apple-system, BlinkMacSystemFont, 'Pretendard', 'Apple Color Emoji', 'Segoe UI Emoji', system-ui, -system-ui, sans-serif",
+}
+
+const container = {
+  margin: '0 auto',
+  padding: '20px',
+  width: '600px',
+}
+
+const heading = {
+  fontSize: '24px',
+  letterSpacing: '-0.5px',
+  lineHeight: '1.3',
+  color: '#484848',
+  padding: '17px 0 0',
+}
+
+const footer = {
+  fontSize: '14px',
+  color: '#b4becc',
+}
+
+const hr = {
+  borderColor: '#dfe1e4',
+  margin: '42px 0 26px',
+}
