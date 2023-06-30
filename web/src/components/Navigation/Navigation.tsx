b225f7e52404115cd795033f5132da13be6f1217
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-06-30 00:04:09 -0700

    Refactor Navigation component and add sidebar functionality

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -1,444 +1,99 @@
-// Base code from https://chakra-templates.dev/
-
-import { useRef, useState, useEffect } from 'react'
-
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  CloseIcon,
-  HamburgerIcon,
-} from '@chakra-ui/icons'
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogOverlay,
-  Avatar,
   Box,
-  Button,
-  Collapse,
-  Flex,
-  Icon,
-  IconButton,
-  Link,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-  Stack,
-  Text,
-  useBreakpointValue,
-  useColorMode,
-  useColorModeValue,
-  useDisclosure,
+  Center,
+  HStack,
+  List,
+  ListItem,
+  ListProps,
 } from '@chakra-ui/react'
-import { SignIn, useUser, useClerk, SignUp } from '@clerk/clerk-react'
-import { useTranslation } from 'react-i18next'
+import { useLocation } from '@redwoodjs/router'
+import { ReactElement, ReactNode } from 'react'
+import { Link as RedwoodLink } from '@redwoodjs/router'
+import { FaHackerNews } from 'react-icons/fa6'
 
-import { Link as RedwoodLink, routes } from '@redwoodjs/router'
-
-const withUnmountFunction = (WrappedComponent, onUnmount) => {
-  return function (props) {
-    useEffect(() => {
-      return () => {
-        if (typeof onUnmount === 'function') {
-          onUnmount()
-        }
-      }
-    }, [])
-    return <WrappedComponent {...props} />
-  }
+export interface RouteItem {
+  title: string
+  path?: string
 }
 
-const WithSubnavigation = () => {
-  const { isOpen, onToggle } = useDisclosure()
-  const { colorMode, toggleColorMode } = useColorMode()
-  const { i18n, t } = useTranslation()
-  const { isSignedIn, user } = useUser()
-  const { signOut } = useClerk()
-  const [signInOpen, setSignInOpen] = useState<boolean>(false)
-  const [signUpOpen, setSignUpOpen] = useState<boolean>(false)
-  const cancelRef = useRef()
-  const SignInWithUnmountFunction = withUnmountFunction(SignIn, () => {
-    setSignInOpen(false)
-  })
-  const SignUpWithUnmountFunction = withUnmountFunction(SignUp, () => {
-    setSignUpOpen(false)
-  })
+export interface Routes {
+  routes: RouteItem[]
+}
+
+export type SidebarContentProps = Routes & {
+  pathname?: string
+  contentRef?: any
+}
+
+type MainNavLinkProps = {
+  href: string
+  icon: ReactElement
+  children: ReactNode
+  label?: string
+}
+
+const MainNavLink = ({ href, icon, children }: MainNavLinkProps) => {
   return (
-    <>
-      {!isSignedIn && (
-        <AlertDialog
-          isOpen={signInOpen}
-          onClose={() => setSignInOpen(false)}
-          leastDestructiveRef={cancelRef}
-        >
-          <AlertDialogOverlay>
-            <AlertDialogContent
-              bg={'rgba(0, 0, 0, 0)'}
-              border={0}
-              shadow={'none'}
-            >
-              <SignInWithUnmountFunction />
-            </AlertDialogContent>
-          </AlertDialogOverlay>
-        </AlertDialog>
-      )}
-      {!isSignedIn && (
-        <AlertDialog
-          isOpen={signUpOpen}
-          onClose={() => setSignUpOpen(false)}
-          leastDestructiveRef={cancelRef}
-        >
-          <AlertDialogOverlay>
-            <AlertDialogContent
-              bg={'rgba(0, 0, 0, 0)'}
-              border={0}
-              shadow={'none'}
-            >
-              <SignUpWithUnmountFunction />
-            </AlertDialogContent>
-          </AlertDialogOverlay>
-        </AlertDialog>
-      )}
-      <Box>
-        <Flex
-          bg={useColorModeValue('white', 'gray.800')}
-          color={useColorModeValue('gray.600', 'white')}
-          minH={'5rem'}
-          maxW={'1200px'}
-          py={{ base: 2 }}
-          px={{ base: 4 }}
-          m={'auto'}
-          align={'center'}
-        >
-          <Flex
-            flex={{ base: 1, md: 'auto' }}
-            ml={{ base: -2 }}
-            display={{ base: 'flex', md: 'none' }}
-          >
-            <IconButton
-              onClick={onToggle}
-              icon={
-                isOpen ? (
-                  <CloseIcon w={3} h={3} />
-                ) : (
-                  <HamburgerIcon w={5} h={5} />
-                )
-              }
-              variant={'ghost'}
-              aria-label={'Toggle Navigation'}
-            />
-          </Flex>
-          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-            <RedwoodLink to={routes.home({ locale: i18n.language })}>
-              <Text
-                textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-                fontFamily={'heading'}
-                color={useColorModeValue('gray.800', 'white')}
-                fontWeight={700}
-              >
-                {t('navigation.logo')}
-              </Text>
-            </RedwoodLink>
-            <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-              <DesktopNav />
-            </Flex>
-          </Flex>
-          <IconButton
-            bg={useColorModeValue('white', 'gray.800')}
-            display={{ base: 'none', md: 'inline-flex' }}
-            onClick={toggleColorMode}
-            m={2}
-            icon={
-              colorMode === 'light' ? (
-                <MoonIcon color="black" />
-              ) : (
-                <SunIcon color="white" />
-              )
-            }
-            aria-label="Toggle Color Mode"
-          />
-          <Stack
-            flex={{ base: 1, md: 0 }}
-            justify={'flex-end'}
-            direction={'row'}
-            spacing={6}
-          >
-            {isSignedIn ? (
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rounded={'full'}
-                  variant={'link'}
-                  cursor={'pointer'}
-                  minW={0}
-                >
-                  <Avatar size={'sm'} src={user?.profileImageUrl} />
-                </MenuButton>
-                <MenuList>
-                  <RedwoodLink to={routes.profile({ locale: i18n.language })}>
-                    <MenuItem>{t('navigation.profile')}</MenuItem>
-                  </RedwoodLink>
-                  <RedwoodLink to={routes.settings({ locale: i18n.language })}>
-                    <MenuItem>{t('navigation.settings')}</MenuItem>
-                  </RedwoodLink>
-                  <MenuDivider />
-                  <Link
-                    onClick={() => signOut()}
-                    textDecor={'none'}
-                    cursor={'pointer'}
-                    _hover={{ textDecor: 'none' }}
-                    minW={0}
-                  >
-                    <MenuItem>{t('navigation.signout')}</MenuItem>
-                  </Link>
-                </MenuList>
-              </Menu>
-            ) : (
-              <>
-                <Button
-                  as={'a'}
-                  fontSize={'sm'}
-                  fontWeight={400}
-                  variant={'link'}
-                  onClick={() => setSignInOpen(!signInOpen)}
-                  cursor={'pointer'}
-                >
-                  {t('navigation.signin')}
-                </Button>
-                <Button
-                  as={'a'}
-                  display={{ base: 'none', md: 'inline-flex' }}
-                  fontSize={'sm'}
-                  fontWeight={600}
-                  color={'white'}
-                  cursor={'pointer'}
-                  bg={'teal.500'}
-                  _hover={{
-                    bg: 'teal.400',
-                  }}
-                  onClick={() => setSignUpOpen(!signInOpen)}
-                >
-                  {t('navigation.signup')}
-                </Button>
-              </>
-            )}
-          </Stack>
-        </Flex>
-        <Collapse in={isOpen} animateOpacity>
-          <MobileNav />
-        </Collapse>
-      </Box>
-    </>
+    <RedwoodLink to={href}>
+      <HStack as="a" spacing="3" fontSize="sm">
+        <Center w="6" h="6" borderWidth="1px" rounded="base">
+          {icon}
+        </Center>
+        <span>{children}</span>
+      </HStack>
+    </RedwoodLink>
   )
 }
 
-const DesktopNav = () => {
-  const linkColor = useColorModeValue('gray.600', 'gray.200')
-  const linkHoverColor = useColorModeValue('gray.800', 'white')
-  const popoverContentBgColor = useColorModeValue('white', 'gray.700')
-  const items = useNavigationItems()
-  return (
-    <Stack direction={'row'} spacing={4}>
-      {items.map((navItem) => (
-        <Box key={navItem.label}>
-          <Popover trigger={'hover'} placement={'bottom-start'}>
-            <PopoverTrigger>
-              <Link
-                p={2}
-                href={navItem.href ?? '#'}
-                fontSize={'sm'}
-                fontWeight={500}
-                color={linkColor}
-                _hover={{
-                  textDecoration: 'none',
-                  color: linkHoverColor,
-                }}
-              >
-                {navItem.label}
-              </Link>
-            </PopoverTrigger>
+export const mainNavLinks = [
+  {
+    icon: <FaHackerNews />,
+    href: '/',
+    label: 'Hacker News Summarized',
+  },
+  {
+    icon: <FaHackerNews />,
+    href: '/',
+    label: 'Hacker News Summarized',
+  },
+  {
+    icon: <FaHackerNews />,
+    href: '/',
+    label: 'Hacker News Summarized',
+  },
+]
 
-            {navItem.children && (
-              <PopoverContent
-                border={0}
-                boxShadow={'xl'}
-                bg={popoverContentBgColor}
-                p={4}
-                rounded={'xl'}
-                minW={'sm'}
-              >
-                <Stack>
-                  {navItem.children.map((child) => (
-                    <DesktopSubNav key={child.label} {...child} />
-                  ))}
-                </Stack>
-              </PopoverContent>
-            )}
-          </Popover>
-        </Box>
+export const MainNavLinkGroup = (props: ListProps) => {
+  return (
+    <List spacing="4" styleType="none" {...props}>
+      {mainNavLinks.map((item) => (
+        <ListItem key={item.label}>
+          <MainNavLink icon={item.icon} href={item.href} label={item.label}>
+            {item.label}
+          </MainNavLink>
+        </ListItem>
       ))}
-    </Stack>
+    </List>
   )
 }
 
-const DesktopSubNav = ({ label, href, sublabel }: NavItem) => {
+const Sidebar = () => {
   return (
-    <Link
-      href={href}
-      role={'group'}
-      display={'block'}
-      p={2}
-      rounded={'md'}
-      _hover={{ bg: useColorModeValue('teal.50', 'gray.900') }}
+    <Box
+      as="nav"
+      pos="sticky"
+      overscrollBehavior="contain"
+      w="280px"
+      h="100%"
+      overflowY="auto"
+      className="sidebar-content"
+      flexShrink={0}
+      border="1px solid red"
+      display={{ base: 'none', md: 'block' }}
     >
-      <Stack direction={'row'} align={'center'}>
-        <Box>
-          <Text
-            transition={'all .3s ease'}
-            _groupHover={{ color: 'teal.500' }}
-            fontWeight={700}
-          >
-            {label}
-          </Text>
-          <Text fontSize={'sm'}>{sublabel}</Text>
-        </Box>
-        <Flex
-          transition={'all .3s ease'}
-          transform={'translateX(-10px)'}
-          opacity={0}
-          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify={'flex-end'}
-          align={'center'}
-          flex={1}
-        >
-          <Icon color={'teal.500'} w={5} h={5} as={ChevronRightIcon} />
-        </Flex>
-      </Stack>
-    </Link>
+      <MainNavLinkGroup mb="10" />
+    </Box>
   )
 }
 
-const MobileNav = () => {
-  const items = useNavigationItems()
-  return (
-    <Stack
-      bg={useColorModeValue('white', 'gray.800')}
-      p={4}
-      display={{ md: 'none' }}
-    >
-      {items.map((navItem) => (
-        <MobileNavItem key={navItem.label} {...navItem} />
-      ))}
-    </Stack>
-  )
-}
-
-const MobileNavItem = ({ label, children, href }: NavItem) => {
-  const { isOpen, onToggle } = useDisclosure()
-
-  return (
-    <Stack spacing={4} onClick={children && onToggle}>
-      <Flex
-        py={2}
-        as={Link}
-        href={href ?? '#'}
-        justify={'space-between'}
-        align={'center'}
-        _hover={{
-          textDecoration: 'none',
-        }}
-      >
-        <Text
-          fontWeight={600}
-          color={useColorModeValue('gray.600', 'gray.200')}
-        >
-          {label}
-        </Text>
-        {children && (
-          <Icon
-            as={ChevronDownIcon}
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
-            w={6}
-            h={6}
-          />
-        )}
-      </Flex>
-
-      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-        <Stack mt={2} pl={4} align={'start'}>
-          {children &&
-            children.map((child) => (
-              <Link key={child.label} py={2} href={child.href}>
-                {child.label}
-              </Link>
-            ))}
-        </Stack>
-      </Collapse>
-    </Stack>
-  )
-}
-
-type NavItem = {
-  label: string
-  sublabel?: string
-  children?: Array<NavItem>
-  href?: string
-}
-
-const useNavigationItems = (): Array<NavItem> => {
-  const { t } = useTranslation()
-  return [
-    {
-      label: t('navigation.explore.title'),
-      children: [
-        {
-          label: t('navigation.explore.hackernews.label'),
-          sublabel: t('navigation.explore.hackernews.sublabel'),
-          href: '',
-        },
-        {
-          label: t('navigation.explore.lobsters.label'),
-          sublabel: t('navigation.explore.lobsters.sublabel'),
-          href: '',
-        },
-        {
-          label: t('navigation.explore.diffblog.label'),
-          sublabel: t('navigation.explore.diffblog.sublabel'),
-          href: '',
-        },
-      ],
-    },
-    {
-      label: t('navigation.personal.title'),
-      children: [
-        {
-          label: t('navigation.personal.industry.label'),
-          sublabel: t('navigation.personal.industry.sublabel'),
-          href: '',
-        },
-        {
-          label: t('navigation.personal.brand.label'),
-          sublabel: t('navigation.personal.brand.sublabel'),
-          href: '',
-        },
-      ],
-    },
-    {
-      label: t('navigation.pricing'),
-      href: '',
-    },
-    {
-      label: t('navigation.support'),
-      href: '',
-    },
-  ]
-}
-
-export default WithSubnavigation
+export default Sidebar
