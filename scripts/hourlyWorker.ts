634081bef56d39f1d66b8e6e31c9f8a53b940f01
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-07-13 16:40:27 -0700

    Update HackerNews stories less than 24 hours old

--- scripts/hourlyWorker.ts
+++ scripts/hourlyWorker.ts
@@ -1,46 +1,85 @@
 import { db } from 'api/src/lib/db'
-import { updateHackerNews } from './updateHackerNews'
+import { log } from './util'
+
+export const updateHackerNews = async () => {
+  let bestStories = await fetch(
+    'https://hacker-news.firebaseio.com/v0/beststories.json'
+  ).then((res) => res.json())
+
+  bestStories = await Promise.all(
+    bestStories.map(
+      async (id) =>
+        await fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        ).then((res) => res.json())
+    )
+  )
+
+  // get stories that are not older than 24 hours
+  bestStories = bestStories.filter((story) => {
+    const now = new Date()
+    const storyDate = new Date(story.time * 1000)
+    const diff = now.getTime() - storyDate.getTime()
+    const diffHours = diff / (1000 * 3600)
+    return diffHours < 24
+  })
+  bestStories.sort((a, b) => b.score - a.score)
+  bestStories = bestStories.slice(0, 5)
+
+  try {
+    await Promise.all(
+      bestStories.map(async (story) => {
+        const id = story.id
+        const url =
+          story.url != null && story.url !== ''
+            ? story.url
+            : `http://news.ycombinator.com/item?id=${id}`
+        const result = await db.summary.upsert({
+          where: {
+            originLink_summaryLocale: {
+              originLink: url,
+              summaryLocale: 'en',
+            },
+          },
+          create: {
+            originLink: url,
+            commentLink:
+              `http://news.ycombinator.com/item?id=${id}` === url ||
+              `https://news.ycombinator.com/item?id=${id}` === url
+                ? ''
+                : `https://news.ycombinator.com/item?id=${id}`,
+            title: story.title,
+            originLocale: 'en',
+            commentLocale: 'en',
+            summaryLocale: 'en',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          update: {
+            updatedAt: new Date(),
+          },
+        })
+        log(`✅ Updated ${result.title}`, 'info')
+      })
+    )
+  } catch (e) {
+    log(e, 'error')
+  }
+}
 
 export default async () => {
-  // for all non-deleted newsletter and has at least one non-deleted subscriber
-
-  const newsletters = await db.newsletter.findMany({
-    where: {
-      deleted: false,
-    },
-  })
-
-  const newsletterIds = newsletters.map((newsletter) => newsletter.id)
-
-  const currentHourInZulu = new Date().getUTCHours()
-
-  const subscribers = await db.subscription.findMany({
-    where: {
-      deleted: false,
-      newsletterId: {
-        in: newsletterIds,
-      },
-      time: {
-        equals: String(currentHourInZulu),
-      },
-    },
-  })
-
-  const newslettersToBeSent = newsletters.filter((newsletter) => {
-    const newsletterSubscribers = subscribers.filter(
-      (subscriber) => subscriber.newsletterId === newsletter.id
-    )
-    return newsletterSubscribers.length > 0
-  })
-
-  // It is guaranteed that HN will exist
+  // fetch HN
   await updateHackerNews()
-  for (const newsletter of newslettersToBeSent) {
-    // update newsletter
-  }
 
-  // generate summary & contents
+  // scrape web
 
-  // send email
+  // populate GPT summary
 
+  // translate contents
+
+  // get all newsletters
+
+  // for each newsletter
+  // if there is an active subscriber
+  // get all summaries that are not older than 24 hours
 }
