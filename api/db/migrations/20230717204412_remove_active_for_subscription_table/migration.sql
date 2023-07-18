a8e862e8fa361174750474ae9331a096e1119f83
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-07-18 14:15:13 -0700

    Remove "active" column from Subscription table

---
+++ api/db/migrations/20230717204412_remove_active_for_subscription_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `active` on the `Subscription` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Subscription" DROP COLUMN "active";
