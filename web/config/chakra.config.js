6fa0d46413374bdb21ddeee063c636bae765b5aa
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-07-27 16:15:04 -0700

    remove web

--- web/config/chakra.config.js
+++
@@ -1,3 +0,0 @@
-// This object will be used to override Chakra-UI theme defaults.
-// See https://chakra-ui.com/docs/styled-system/theming/theme for theming options
-module.exports = {}
--- web/src/i18next-parser.config.ts
+++
@@ -1,6 +0,0 @@
-// import { allowedLocalesList } from './i18n'
-
-export default {
-  locales: ['en', 'ko'],
-  sort: true,
-}
