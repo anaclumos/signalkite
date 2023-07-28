ef5ff04af88b9c9ff7e23441a9170d587925ba24
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-07-27 21:55:41 -0700

    Update project title and tagline, change URL and organization name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,24 +6,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Heimdall',
+  tagline: 'Hacker News in All Languages',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://hn.cho.sh',
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'anaclumos', // Usually your GitHub org/user name.
+  projectName: 'heimdall', // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
+  trailingSlash: false,
   i18n: {
     defaultLocale: 'en',
     locales: [
