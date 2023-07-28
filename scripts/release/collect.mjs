079388fc5cfe4e0eed69808198f814ea42e9055c
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-07-27 22:07:22 -0700

    compile release

---
+++ scripts/release/collect.mjs
@@ -0,0 +1,69 @@
+import { log, sanitize } from './util.mjs';
+import { YoutubeTranscript } from 'youtube-transcript';
+import puppeteer from 'puppeteer-core';
+export const collect = async (url) => {
+    if (url === '')
+        return '';
+    let body = '';
+    // 1. Youtube
+    if (url?.includes('youtu')) {
+        try {
+            const videoId = url.split('v=')[1].split('&')[0] ??
+                url.split('youtu.be/')[1].split('&')[0] ??
+                url.split('youtube.com/embed/')[1].split('&')[0] ??
+                url.split('youtube.com/watch?v=')[1].split('&')[0];
+            const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+            body = transcript.map((t) => t.text).join(' ');
+            if (body?.toString().trim() === '') {
+                throw new Error('😵 Youtube Transcript is empty');
+            }
+        }
+        catch (e) {
+            log(`❌ Error\tCannot Download Youtube Transcript for ${url}`, 'error');
+        }
+    }
+    if (url?.includes('twitter')) {
+        url = `https://publish.twitter.com/oembed?url=${encodeURIComponent(url)}`;
+        body = await fetch(url)
+            .then((r) => r.json())
+            .then((r) => r.html);
+    }
+    if (body === '') {
+        try {
+            log(`⏳ Downloading\t ${url}`, 'info');
+            let browser = null;
+            try {
+                const browser = await puppeteer.connect({
+                    browserWSEndpoint: `wss://${process.env.BRIGHTDATA_USERNAME}:${process.env.BRIGHTDATA_PASSWORD}@${process.env.BRIGHTDATA_PROXY}`,
+                });
+                const page = await browser.newPage();
+                page.setDefaultNavigationTimeout(2 * 60 * 1000);
+                await page.goto(url);
+                await page.waitForSelector('body');
+                const article = await page.$('article');
+                if (article) {
+                    body = await page.$eval('article', (el) => el.innerText);
+                }
+                const main = await page.$('main');
+                if (main) {
+                    body = await page.$eval('main', (el) => el.innerText);
+                }
+                const bodyEl = await page.$('body');
+                if (bodyEl) {
+                    body = await page.$eval('body', (el) => el.innerText);
+                }
+            }
+            catch (e) {
+                console.error('run failed', e);
+            }
+            finally {
+                await browser?.close();
+            }
+            log(`✅ Downloaded\t BrightData for ${url}`, 'info');
+        }
+        catch (e) {
+            log(`❌ Error\t Cannot Download BrightData for ${url}\n${e}`, 'info');
+        }
+    }
+    return sanitize(body);
+};
---
+++ scripts/release/update.mjs
@@ -0,0 +1,46 @@
+import { HACKER_NEWS_PER_HOUR } from './config.mjs';
+/**
+ * Update Hacker News
+ * Returns the top 5 stories from Hacker News that are not older than 24 hours
+ * @returns {Promise<[]>}
+ * @example
+ * const bestStories = await updateHN()
+ */
+export const updateHN = async () => {
+    let hnResponse = await fetch('https://hacker-news.firebaseio.com/v0/beststories.json').then((res) => res.json());
+    let bestStories = await Promise.all(hnResponse.map(async (id, idx) => {
+        await new Promise((resolve) => setTimeout(resolve, 10 * idx));
+        const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then((res) => {
+            if (res.status === 200) {
+                return res.json();
+            }
+            else {
+                throw new Error(`Hacker News API returned ${res.status}`);
+            }
+        });
+        return {
+            id: response.id,
+            title: response.title,
+            originLink: response.url,
+            originBody: response.text,
+            commentLink: `https://news.ycombinator.com/item?id=${response.id}`,
+            commentBody: '',
+            originSummary: [],
+            commentSummary: [],
+            points: response.score,
+            commentCount: response.descendants,
+            retryCount: 0,
+            time: response.time,
+        };
+    }));
+    bestStories = bestStories.filter((story) => {
+        const now = new Date();
+        const storyDate = new Date(story.time * 1000);
+        const diff = now.getTime() - storyDate.getTime();
+        const diffHours = diff / (1000 * 3600);
+        return diffHours < 24;
+    });
+    bestStories.sort((a, b) => b.points - a.points);
+    bestStories = bestStories.slice(0, HACKER_NEWS_PER_HOUR);
+    return bestStories;
+};
