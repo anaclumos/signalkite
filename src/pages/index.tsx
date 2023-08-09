ba072256c2fbbb0c2e9bbd229060bffda3f7ab32
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-08-08 17:24:33 -0700

    Update index.module.css and index.tsx with new styles and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,58 @@
 import React from 'react'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
-import { Sub } from '../components/Sub'
+import Translate from '@docusaurus/Translate'
+import Link from '@docusaurus/Link'
+import styles from './index.module.css'
+import clsx from 'clsx'
+import { Globe } from '../components/Cobe'
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
-      <main>
-        <Sub />
+      <main className={styles.main}>
+        <div className={styles.hero}>
+<div className={styles.heroElement}>
+<h1>
+<Translate>
+      Heimdall
+</Translate>
+</h1>
+
+<ul className={styles.features}>
+  <li className={styles.bullet}>
+  <Translate>
+Heimdall summarizes Hacker News in 30 Languages.
+</Translate>
+  </li>
+  <li className={styles.bullet}>
+  <Translate>
+Trusted by Professionals at Apple, Microsoft, Amazon, and More.
+</Translate>
+  </li>
+  <li className={styles.bullet}>
+  <Translate>
+No Hidden Fees. No Premium.
+</Translate>
+  </li>
+
+</ul>
+<Link to="/subscribe" className={clsx('button button--primary button--lg', styles.subscribeButton)}>
+<Translate>
+
+      Subscribe
+</Translate>
+</Link>
+<Link to={"/today"} className={clsx('button button--secondary button--lg', styles.readFirstButton)}>
+  Read First
+</Link>
+        </div>
+</div>
+
+        <div className={styles.background}>
+<Globe />
+          </div>
       </main>
     </Layout>
   )
