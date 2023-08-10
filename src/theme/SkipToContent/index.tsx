e132e99cc7fb7e48bd0f7cbb346f56f0683060e5
Sunghyun Cho <sunghyun.cho@grammarly.com>
2023-08-10 10:05:18 -0700

    Format documents

--- src/theme/SkipToContent/index.tsx
+++ src/theme/SkipToContent/index.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import SkipToContent from '@theme-original/SkipToContent';
-import type SkipToContentType from '@theme/SkipToContent';
-import type {WrapperProps} from '@docusaurus/types';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { useShare } from '@site/static/hooks/useShare';
+import React from 'react'
+import SkipToContent from '@theme-original/SkipToContent'
+import type SkipToContentType from '@theme/SkipToContent'
+import type { WrapperProps } from '@docusaurus/types'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import { useShare } from '@site/static/hooks/useShare'
 
-type Props = WrapperProps<typeof SkipToContentType>;
+type Props = WrapperProps<typeof SkipToContentType>
 
 export default function SkipToContentWrapper(props: Props): JSX.Element {
+  const { i18n } = useDocusaurusContext()
 
-  const {i18n} = useDocusaurusContext();
-
-  useShare(i18n.currentLocale);
+  useShare(i18n.currentLocale)
 
   return (
     <>
       <SkipToContent {...props} />
     </>
-  );
+  )
 }
