d80fa3a9bdc20dcd63d4ee759a6237de6ebc4ca2
Sunghyun Cho <hey@cho.sh>
2023-11-06 13:26:05 -0800

    feat: add relate ad

---
+++ scripts/relateStoryAd.ts
@@ -0,0 +1,272 @@
+export const relateStoryAd = {
+    "ar": {
+        "title": "Relate - CRM مبيعات حديث للمؤسسين الذين لديهم خبرة مبيعات 0",
+        "summary": [
+            "Relate (YC S22) هو CRM مبيعات حديث للشركات الناشئة B2B المبكرة.",
+            "أراد الفريق المؤسس CRM سهل الاستخدام يعمل خارج الصندوق ولكن لم يتمكن من العثور على واحد في السوق. لذلك قاموا ببناء واحدة للمؤسسين الذين يرتدون أحذية مماثلة.",
+            "لا تحتاج إلى أي خبرة أو معرفة في المبيعات لبدء استخدام Relate. الأفضل لمؤسسي B2B الذين بدأوا للتو."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "bn": {
+        "title": "সম্পর্কিত - 0 বিক্রয় অভিজ্ঞতা সহ প্রতিষ্ঠাতাদের জন্য একটি আধুনিক বিক্রয় সিআরএম",
+        "summary": [
+            "রিলেট (ওয়াইসি এস 22) প্রাথমিক বি 2 বি স্টার্টআপগুলির জন্য একটি আধুনিক বিক্রয় সিআরএম।",
+            "প্রতিষ্ঠাতা দলটি একটি সহজে ব্যবহারযোগ্য সিআরএম চেয়েছিল যা বাক্সের বাইরে কাজ করে তবে বাজারে এটি খুঁজে পায়নি। সুতরাং তারা প্রতিষ্ঠাতাদের জন্য একটি তৈরি করেছে যারা অনুরূপ জুতা পরেআছে।",
+            "রিলেট দিয়ে শুরু করার জন্য আপনার কোনও বিক্রয় অভিজ্ঞতা বা জ্ঞানের প্রয়োজন নেই। B2B প্রতিষ্ঠাতাদের জন্য সেরা যারা সবেমাত্র শুরু করছেন।"
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "cs": {
+        "title": "Relate - moderní prodejní CRM pro zakladatele s nulovými zkušenostmi s prodejem",
+        "summary": [
+            "Relate (YC S22) je moderní prodejní CRM pro začínající B2B firmy.",
+            "Zakladatelský tým chtěl snadno použitelný CRM, který by fungoval ihned po vybalení z krabice, ale nemohl žádný na trhu najít. Proto ho vytvořili pro zakladatele, kteří jsou na tom podobně.",
+            "Abyste mohli začít pracovat se společností Relate, nepotřebujete žádné zkušenosti ani znalosti v oblasti prodeje. Nejlepší pro zakladatele B2B, kteří teprve začínají."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "da": {
+        "title": "Relate - et moderne salgs-CRM til grundlæggere med 0 års salgserfaring",
+        "summary": [
+            "Relate (YC S22) er et moderne salgs-CRM til tidlige B2B-startups.",
+            "Grundlæggerteamet ville have et brugervenligt CRM, der fungerer lige ud af landevejen, men kunne ikke finde et på markedet. Så de byggede et til grundlæggere, der er i samme situation.",
+            "Du behøver ingen salgserfaring eller viden for at komme i gang med Relate. Bedst til B2B-stiftere, der lige er kommet i gang."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "de": {
+        "title": "Relate - ein modernes Vertriebs-CRM für Gründer mit 0 Vertriebserfahrung",
+        "summary": [
+            "Relate (YC S22) ist ein modernes Vertriebs-CRM für junge B2B-Startups.",
+            "Das Gründerteam wollte ein benutzerfreundliches, sofort einsetzbares CRM, konnte aber keins auf dem Markt finden. Also bauten sie eines für Gründer, die sich in ähnlichen Situationen befinden.",
+            "Sie brauchen keine Vertriebserfahrung oder Kenntnisse, um mit Relate zu beginnen. Am besten für B2B-Gründer, die gerade erst anfangen."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "el": {
+        "title": "Relate - ένα σύγχρονο CRM πωλήσεων για ιδρυτές με 0 εμπειρία στις πωλήσεις",
+        "summary": [
+            "Το Relate (YC S22) είναι ένα σύγχρονο CRM πωλήσεων για νεοσύστατες επιχειρήσεις B2B.",
+            "Η ιδρυτική ομάδα ήθελε ένα εύχρηστο CRM που να λειτουργεί από το κουτί, αλλά δεν μπορούσε να βρει ένα τέτοιο στην αγορά. Έτσι δημιούργησαν ένα για τους ιδρυτές που βρίσκονται σε παρόμοια θέση.",
+            "Δεν χρειάζεστε εμπειρία ή γνώσεις πωλήσεων για να ξεκινήσετε με το Relate. Το καλύτερο για ιδρυτές B2B που μόλις ξεκινούν."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "en": {
+        "title": "Relate — a modern sales CRM for founders with 0 sales experience",
+        "summary": [
+            "Relate (YC S22) is a modern sales CRM for early B2B startups.",
+            "The founding team wanted an easy-to-use CRM that works out of the box but couldn't find one on the market. So they built one for founders who are in similar shoes.",
+            "You don't need any sales experience or knowledge to get started with Relate. Best for B2B founders who are just getting started."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "es": {
+        "title": "Relate - un CRM de ventas moderno para fundadores con 0 experiencia en ventas",
+        "summary": [
+            "Relate (YC S22) es un CRM de ventas moderno para las primeras startups B2B.",
+            "El equipo fundador quería un CRM fácil de usar y que funcionara a la primera, pero no lo encontró en el mercado. Así que crearon uno para los fundadores que se encuentran en una situación similar.",
+            "No necesitas experiencia o conocimientos de ventas para empezar con Relate. Lo mejor para fundadores B2B que acaban de empezar."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "fi": {
+        "title": "Relate - moderni myynnin CRM perustajille, joilla ei ole kokemusta myynnistä.",
+        "summary": [
+            "Relate (YC S22) on nykyaikainen myynnin CRM-järjestelmä varhaisessa vaiheessa oleville B2B-startup-yrityksille.",
+            "Perustajatiimi halusi helppokäyttöisen CRM:n, joka toimisi heti alusta alkaen, mutta ei löytänyt sellaista markkinoilta. Niinpä he rakensivat sellaisen samanlaisessa tilanteessa oleville perustajille.",
+            "Et tarvitse myyntikokemusta tai -tietämystä päästäksesi alkuun Relaten kanssa. Sopii parhaiten B2B-perustajille, jotka ovat vasta aloittamassa."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "fr": {
+        "title": "Relate - un CRM moderne pour les fondateurs n'ayant aucune expérience de la vente",
+        "summary": [
+            "Relate (YC S22) est une solution moderne de gestion de la relation client (CRM) pour les jeunes entreprises B2B.",
+            "L'équipe fondatrice souhaitait un CRM facile à utiliser et prêt à l'emploi, mais n'en trouvait pas sur le marché. Ils en ont donc créé un pour les fondateurs qui se trouvent dans la même situation qu'eux.",
+            "Vous n'avez pas besoin d'expérience ou de connaissances en matière de vente pour démarrer avec Relate. Idéal pour les fondateurs d'entreprises B2B qui débutent."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "he": {
+        "title": "Relate — CRM מכירות מודרני למייסדים עם 0 ניסיון במכירות",
+        "summary": [
+            "Relate (YC S22) הוא CRM מכירות מודרני לסטארט-אפים B2B מוקדמים.",
+            "צוות המייסדים רצה CRM קל לשימוש שעובד מהקופסה אך לא הצליח למצוא אחד בשוק. אז הם בנו אחד למייסדים שנמצאים בנעליים דומות.",
+            "אינך זקוק לשום ניסיון או ידע במכירות כדי להתחיל לעבוד עם Relate. הטוב ביותר עבור מייסדי B2B שרק מתחילים."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "hi": {
+        "title": "संबंधित - 0 बिक्री अनुभव वाले संस्थापकों के लिए एक आधुनिक बिक्री सीआरएम",
+        "summary": [
+            "संबंधित (वाईसी एस 22) शुरुआती बी 2 बी स्टार्टअप के लिए एक आधुनिक बिक्री सीआरएम है।",
+            "संस्थापक टीम एक आसान उपयोग सीआरएम चाहती थी जो बॉक्स से बाहर काम करती है लेकिन बाजार पर एक नहीं मिल सका। इसलिए उन्होंने संस्थापकों के लिए एक बनाया जो समान जूते में हैं।",
+            "Relate के साथ आरंभ करने के लिए आपको किसी भी बिक्री अनुभव या ज्ञान की आवश्यकता नहीं है। बी 2 बी संस्थापकों के लिए सबसे अच्छा जो अभी शुरू कर रहे हैं।"
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "hu": {
+        "title": "Relate - egy modern értékesítési CRM 0 értékesítési tapasztalattal rendelkező alapítók számára",
+        "summary": [
+            "A Relate (YC S22) egy modern értékesítési CRM a korai B2B startupok számára.",
+            "Az alapító csapat egy könnyen használható, azonnal működő CRM-et akart, de nem találtak ilyet a piacon. Ezért építettek egyet a hasonló cipőben járó alapítók számára.",
+            "Nincs szükséged értékesítési tapasztalatra vagy ismeretekre ahhoz, hogy elkezdhesd a Relate-tel. A legjobb B2B alapítók számára, akik még csak most kezdik."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "id": {
+        "title": "Relate - CRM penjualan modern untuk para pendiri dengan 0 pengalaman penjualan",
+        "summary": [
+            "Relate (YC S22) adalah CRM penjualan modern untuk perusahaan rintisan B2B.",
+            "Tim pendiri menginginkan CRM yang mudah digunakan yang bekerja di luar kebiasaan tetapi tidak dapat menemukannya di pasaran. Jadi mereka membuat satu untuk para pendiri yang memiliki masalah serupa.",
+            "Anda tidak memerlukan pengalaman atau pengetahuan penjualan apa pun untuk memulai dengan Relate. Cocok untuk pendiri B2B yang baru saja memulai."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "it": {
+        "title": "Relate - un moderno CRM di vendita per fondatori con zero esperienza di vendita",
+        "summary": [
+            "Relate (YC S22) è un moderno CRM di vendita per le prime startup B2B.",
+            "Il team di fondatori voleva un CRM facile da usare e che funzionasse subito, ma non riusciva a trovarne uno sul mercato. Così ne hanno costruito uno per i fondatori che si trovano in situazioni simili.",
+            "Non è necessaria alcuna esperienza o conoscenza delle vendite per iniziare con Relate. Ideale per i fondatori B2B che hanno appena iniziato."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "ja": {
+        "title": "Relate - 営業経験ゼロの創業者のためのモダンな営業CRM",
+        "summary": [
+            "Relate (YC S22)は、初期のB2Bスタートアップ向けのモダンなセールスCRMです。",
+            "創業チームは、すぐに使える使いやすいCRMを望んでいたが、市販されているものは見つからなかった。そこで彼らは、同じような境遇にある創業者のためにCRMを構築しました。",
+            "Relateを使い始めるのに、営業の経験や知識は必要ありません。B2Bを始めたばかりの創業者に最適です。"
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "ko": {
+        "title": "Relate - 세일즈 경험이 없는 개발자 출신 B2B 창업자도 사용할 수 있는 쉬운 CRM",
+        "summary": [
+            "Relate (YC S22)는 초기 B2B 스타트업을 위한 심플한 세일즈 CRM 입니다.",
+            "Relate 창업팀은 배우지 않고 사용할 수 있는 쉬운 CRM이 필요했지만 시중에서는 찾을 수 없었습니다. 그래서 직접 CRM을 만들었다고 합니다.",
+            "세일즈 경험이나 지식이 없어도 Relate에 바로 가입해서 영업관리를 쉽게 시작할 수 있습니다."
+        ],
+        "link": "https://relate.kr/?ref=heimdall"
+    },
+    "nl": {
+        "title": "Relate - een modern verkoop-CRM voor oprichters met 0 verkoopervaring",
+        "summary": [
+            "Relate (YC S22) is een modern verkoop-CRM voor startende B2B bedrijven.",
+            "Het oprichtersteam wilde een gebruiksvriendelijk CRM dat kant-en-klaar werkt, maar kon er geen vinden op de markt. Dus bouwden ze er een voor oprichters die in dezelfde schoenen staan.",
+            "Je hebt geen verkoopervaring of -kennis nodig om met Relate aan de slag te gaan. Het beste voor B2B-oprichters die net beginnen."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "nb": {
+        "title": "Relate - et moderne salgs-CRM for gründere med null salgserfaring",
+        "summary": [
+            "Relate (YC S22) er et moderne salgs-CRM for nystartede B2B-virksomheter.",
+            "Gründerteamet ønsket seg et brukervennlig CRM-system som fungerer umiddelbart, men fant ikke noe på markedet. Derfor utviklet de en løsning for gründere i samme situasjon.",
+            "Du trenger ingen salgserfaring eller kunnskap for å komme i gang med Relate. Best for B2B-gründere som akkurat har kommet i gang."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "pl": {
+        "title": "Relate - nowoczesny CRM sprzedażowy dla założycieli z zerowym doświadczeniem w sprzedaży",
+        "summary": [
+            "Relate (YC S22) to nowoczesny CRM sprzedażowy dla wczesnych startupów B2B.",
+            "Zespół założycielski chciał łatwego w użyciu CRM, który działa od razu po wyjęciu z pudełka, ale nie mógł znaleźć takiego na rynku. Stworzyli go więc dla założycieli, którzy są w podobnej sytuacji.",
+            "Aby rozpocząć korzystanie z Relate, nie trzeba mieć żadnego doświadczenia ani wiedzy w zakresie sprzedaży. Najlepszy dla założycieli B2B, którzy dopiero zaczynają."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "pt": {
+        "title": "Relate - um CRM de vendas moderno para fundadores com nenhuma experiência em vendas",
+        "summary": [
+            "O Relate (YC S22) é um CRM de vendas moderno para as primeiras startups B2B.",
+            "A equipe fundadora queria um CRM fácil de usar que funcionasse imediatamente, mas não conseguia encontrar um no mercado. Então, eles criaram um para os fundadores que estavam em situação semelhante.",
+            "Você não precisa de nenhuma experiência ou conhecimento de vendas para começar a usar o Relate. Ideal para fundadores B2B que estão apenas começando."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "ro": {
+        "title": "Relate - un CRM de vânzări modern pentru fondatorii cu 0 experiență în vânzări",
+        "summary": [
+            "Relate (YC S22) este un CRM de vânzări modern pentru startup-urile B2B aflate la început de drum.",
+            "Echipa fondatoare dorea un CRM ușor de utilizat, care să funcționeze din start, dar nu a găsit unul pe piață. Așa că au construit unul pentru fondatorii care se află în aceeași situație.",
+            "Nu aveți nevoie de experiență sau cunoștințe în domeniul vânzărilor pentru a începe cu Relate. Cel mai bun pentru fondatorii B2B care sunt la început de drum."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "ru": {
+        "title": "Relate - современная CRM для основателей с нулевым опытом продаж",
+        "summary": [
+            "Relate (YC S22) - современная CRM продаж для ранних B2B-стартапов.",
+            "Команда основателей хотела иметь простую в использовании CRM, работающую \"из коробки\", но не смогла найти такую на рынке. Поэтому они создали такую систему для основателей, которые находятся в похожей ситуации.",
+            "Для того чтобы начать работу с Relate, не требуется опыта и знаний в области продаж. Лучше всего подходит для основателей B2B, которые только начинают свою деятельность."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "sk": {
+        "title": "Relate - moderný predajný CRM pre zakladateľov s 0 skúsenosťami s predajom",
+        "summary": [
+            "Relate (YC S22) je moderný predajný CRM pre začínajúce B2B startupy.",
+            "Zakladateľský tím chcel ľahko použiteľný CRM, ktorý by fungoval hneď po vybalení, ale na trhu ho nenašiel. Preto ho vytvorili pre zakladateľov, ktorí sú na tom podobne.",
+            "Na to, aby ste mohli začať pracovať so spoločnosťou Relate, nepotrebujete žiadne skúsenosti ani znalosti v oblasti predaja. Najlepšie pre zakladateľov B2B, ktorí práve začínajú."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "sv": {
+        "title": "Relate - ett modernt CRM-system för grundare med 0 års erfarenhet av försäljning",
+        "summary": [
+            "Relate (YC S22) är ett modernt CRM-system för försäljning för nystartade B2B-företag.",
+            "Grundarteamet ville ha ett lättanvänt CRM-system som fungerar direkt, men kunde inte hitta något på marknaden. Så de byggde ett för grundare som var i samma situation.",
+            "Du behöver ingen erfarenhet av försäljning eller kunskap för att komma igång med Relate. Bäst för B2B-grundare som precis har kommit igång."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "ta": {
+        "title": "தொடர்புடையது - 0 விற்பனை அனுபவம் கொண்ட நிறுவனர்களுக்கான நவீன விற்பனை CRM",
+        "summary": [
+            "ரிலேட் (YC S22) என்பது ஆரம்பகால B2B ஸ்டார்ட்அப்களுக்கான ஒரு நவீன விற்பனை CRM ஆகும்.",
+            "நிறுவனக் குழு பயன்படுத்த எளிதான சிஆர்எம்-ஐ விரும்பியது, இது பெட்டியிலிருந்து வேலை செய்கிறது, ஆனால் சந்தையில் ஒன்றைக் கண்டுபிடிக்க முடியவில்லை. எனவே அவர்கள் ஒத்த காலணிகளில் இருக்கும் நிறுவனர்களுக்காக ஒன்றை உருவாக்கினர்.",
+            "ரிலேட்டுடன் தொடங்க உங்களுக்கு எந்த விற்பனை அனுபவமோ அல்லது அறிவோ தேவையில்லை. இப்போது தொடங்கும் பி 2 பி நிறுவனர்களுக்கு சிறந்தது."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "th": {
+        "title": "เกี่ยวข้อง — CRM การขายที่ทันสมัยสําหรับผู้ก่อตั้งที่มีประสบการณ์การขาย 0",
+        "summary": [
+            "Relate (YC S22) เป็น CRM การขายที่ทันสมัยสําหรับสตาร์ทอัพ B2B รุ่นแรก",
+            "ทีมผู้ก่อตั้งต้องการ CRM ที่ใช้งานง่ายซึ่งใช้งานได้ทันที แต่ไม่สามารถหาได้ในตลาด ดังนั้นพวกเขาจึงสร้างหนึ่งสําหรับผู้ก่อตั้งที่อยู่ในรองเท้าที่คล้ายกัน",
+            "คุณไม่จําเป็นต้องมีประสบการณ์การขายหรือความรู้ใด ๆ เพื่อเริ่มต้นใช้งาน Relate ดีที่สุดสําหรับผู้ก่อตั้ง B2B ที่เพิ่งเริ่มต้น"
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "tr": {
+        "title": "Relate - 0 satış deneyimi olan kurucular için modern bir satış CRM'i",
+        "summary": [
+            "Relate (YC S22), erken dönem B2B girişimleri için modern bir satış CRM'idir.",
+            "Kurucu ekip, kutudan çıktığı gibi çalışan, kullanımı kolay bir CRM istiyordu ancak piyasada böyle bir CRM bulamadılar. Bu yüzden benzer durumda olan kurucular için bir CRM geliştirdiler.",
+            "Relate ile başlamak için herhangi bir satış deneyimine veya bilgisine ihtiyacınız yok. Yeni başlayan B2B kurucuları için en iyisi."
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "zh-Hans": {
+        "title": "Relate--专为没有销售经验的创始人设计的现代销售 CRM",
+        "summary": [
+            "Relate (YC S22) 是一款面向早期 B2B 创业公司的现代销售 CRM。",
+            "创始团队希望有一个简单易用、开箱即用的 CRM，但在市场上找不到。于是，他们为有类似需求的创始人打造了一款。",
+            "您无需任何销售经验或知识即可开始使用 Relate。最适合刚刚起步的 B2B 创始人。"
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    },
+    "zh-Hant": {
+        "title": "Relate — 面向 0 銷售經驗的創始人的現代銷售 CRM",
+        "summary": [
+            "Relate （YC S22） 是一款面向早期 B2B 初創公司的現代銷售 CRM。",
+            "創始團隊想要一個易於使用的CRM，開箱即用，但在市場上找不到。因此，他們為處於類似地位的創始人建立了一個。",
+            "您不需要任何銷售經驗或知識即可開始使用 Relate。最適合剛起步的 B2B 創始人。"
+        ],
+        "link": "https://relate.so/?ref=heimdall"
+    }
+}
