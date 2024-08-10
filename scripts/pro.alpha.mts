4d767361836e59fa44bae59a2e555bf477469aea
Sunghyun Cho <hey@cho.sh>
2024-08-10 15:13:07 +0900

    SignalKite

--- scripts/pro.alpha.mts
+++
@@ -1,8 +0,0 @@
-export const subscriptions = [
-  {
-    query: '전문연구요원',
-    serachResultLang: 'ko',
-    locale: 'ko',
-    email: 'anaclumos@gmail.com',
-  },
-]
--- src/theme/Icon/ExternalLink/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styles from './styles.module.css'
-
-export default function IconExternalLink(): JSX.Element {
-  return <span className={styles.iconExternalLink}></span>
-}
