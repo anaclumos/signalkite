3ef047745569042c63aaad695709e4d26204d038
Sunghyun Cho <hey@cho.sh>
2024-08-11 18:43:26 +0900

    chore: Remove Storybook configuration files

--- apps/storybook.signalkite.com/tailwind.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("@signalkite/tailwind/tailwind.config")
--- apps/storybook.signalkite.com/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
-
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
