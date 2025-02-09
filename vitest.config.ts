203c8443e5c89fd0e8c0ca494aa7f569f21d88a2
Sunghyun Cho <hey@cho.sh>
2025-02-10 00:22:32 +0900

    :sparkles: (vitest) exclude e2e tests from vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     environment: "node",
     include: ["**/*.spec.ts"],
+    exclude: ["e2e/**"],
     setupFiles: ["./lib/setup.ts"],
     alias: {
       "@": path.resolve(__dirname, "./"),
