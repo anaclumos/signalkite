ba0c79637e5f3ee3fc5e2b696f7c01eec579fcd4
Sunghyun Cho <hey@cho.sh>
2025-02-11 23:25:59 +0900

    :art: cancel and submit button placement

--- components/entity-form.tsx
+++ components/entity-form.tsx
@@ -60,31 +60,40 @@ export function EntityForm({
       <NavBar
         breadcrumbs={breadcrumbs}
         actions={
-          entityId &&
-          onDelete && (
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="destructive">Delete</Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Delete {title}</DialogTitle>
-                  <DialogDescription>
-                    Are you sure you want to delete this {title.toLowerCase()}?
-                    This action cannot be undone.
-                  </DialogDescription>
-                </DialogHeader>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="secondary">Cancel</Button>
-                  </DialogClose>
-                  <form action={() => onDelete(entityId)}>
-                    <Button variant="destructive">Delete</Button>
-                  </form>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          )
+          <>
+            {entityId && onDelete && (
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="destructive">Delete</Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Delete {title}</DialogTitle>
+                    <DialogDescription>
+                      Are you sure you want to delete this {title.toLowerCase()}
+                      ? This action cannot be undone.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant="secondary">Cancel</Button>
+                    </DialogClose>
+                    <form action={() => onDelete(entityId)}>
+                      <Button variant="destructive">Delete</Button>
+                    </form>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            )}
+            <div className="flex items-center justify-end gap-2">
+              <Link href={backUrl}>
+                <Button variant="secondary" type="button">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit">{submitLabel}</Button>
+            </div>
+          </>
         }
       />
 
@@ -107,15 +116,6 @@ export function EntityForm({
         ))}
 
         <Divider />
-
-        <div className="flex items-center justify-end space-x-4 p-4 md:p-8">
-          <Link href={backUrl}>
-            <Button variant="secondary" type="button">
-              Cancel
-            </Button>
-          </Link>
-          <Button type="submit">{submitLabel}</Button>
-        </div>
       </form>
     </>
   )
