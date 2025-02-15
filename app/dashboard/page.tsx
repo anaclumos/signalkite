ee64e45134a88c8cbf7538e7c1376d9fca5eccaf
Sunghyun Cho <hey@cho.sh>
2025-02-15 23:58:03 +0900

    style: update construction message in Archives, Dashboard, and Marketplace pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-4">
       <h1 className="text-5xl font-bold text-center">Dashboard</h1>
-      <p className="text-center text-gray-500">...is under construction!</p>
+      <p className="text-center text-gray-500">under construction</p>
     </div>
   )
 }
