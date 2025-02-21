6f29f724fe6c4538d5df9e218cbb506d6317ab94
Sunghyun Cho <hey@cho.sh>
2025-02-21 18:51:08 +0900

    refactor: remove noti channel syncing from clerk

--- app/notification-channels/page.tsx
+++ app/notification-channels/page.tsx
@@ -9,65 +9,6 @@ export default async function NotificationChannelsPage() {
     return null
   }
 
-  // Ensure user exists in our database
-  await db.user.upsert({
-    where: { authProviderUid: user.id },
-    create: {
-      authProviderUid: user.id,
-    },
-    update: {},
-  })
-
-  const emails = user.emailAddresses.map((email) => ({
-    id: email.id,
-    email: email.emailAddress,
-  }))
-  const phones = user.phoneNumbers.map((phone) => ({
-    id: phone.id,
-    phone: phone.phoneNumber,
-  }))
-
-  emails.forEach(async (email) => {
-    await db.notificationChannel.upsert({
-      where: { clerkId: email.id },
-      create: {
-        name: `Email - ${email.email}`,
-        type: "email",
-        settings: { email: email.email },
-        clerkId: email.id,
-        user: {
-          connect: {
-            authProviderUid: user.id,
-          },
-        },
-      },
-      update: {
-        settings: { email: email.email },
-      },
-    })
-  })
-
-  phones.forEach(async (phone) => {
-    await db.notificationChannel.upsert({
-      where: { clerkId: phone.id },
-      create: {
-        name: `TEXT - ${phone.phone}`,
-        type: "TEXT",
-        settings: { phone: phone.phone },
-        clerkId: phone.id,
-        user: {
-          connect: {
-            authProviderUid: user.id,
-          },
-        },
-      },
-      update: {
-        settings: { phone: phone.phone },
-      },
-    })
-  })
-
-  // Get all channels for this user with relations
   const channels = await db.notificationChannel.findMany({
     where: {
       user: {
